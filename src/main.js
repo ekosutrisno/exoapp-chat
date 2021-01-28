b2a51c5ebc1e00fe14b6875493c26325f8e37732
ekosutrisno <51039205+ekosutrisno@users.noreply.github.com>
2021-01-28 10:10:38 +0700

    config: add depedency momentjs and vue-click-away

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VueClickAway from "vue3-click-away";
 import './index.css';
 
 createApp(App)
    .use(router)
    .use(store)
+   .use(VueClickAway)
    .mount('#app');
